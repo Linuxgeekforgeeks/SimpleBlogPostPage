d76e78be0ad1e9076d916e26c86c8e1a4ed21f7a
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-04-17 20:03:49 -0700

    able to fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useState } from "react";
-import "./App.css"
-import PostPage from "./pages/postPage/PostPage"
+import { useEffect, useState } from "react";
+import "./App.css";
+import PostPage from "./pages/postPage/PostPage";
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'First Post', content: 'This is the content of the first post.', category: 'Technology' },
-    { id: 2, title: 'Second Post', content: 'This is the content of the second post.', category: 'Travel' },
-    { id: 3, title: 'Third Post', content: 'This is the content of the third post.', category: 'Science' },
-    {id:4,title:"Firth Post",content:"This is the fourth posts.",category:"Travel"},
-    {id:5,title:"Sixth Post",content:"This is the six posts.",category:"Science"},
-    {id:6,title:"Fourth Post",content:"This is the fourth posts.",category:"Travel"}
-]);
+  const [posts, setPosts] = useState([]);
+  const [Loading, setLoading] = useState(true);
+
+  const fetchPosts=async()=>{
+    try {
+      const postsdata= await fetch("http://localhost:4000/posts").then(res=>res.json())
+    console.log(postsdata)
+    setPosts(postsdata)
+    setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+
+  useEffect(()=>{
+    fetchPosts();
+  },[])
+
+if(Loading){
+  return <h1>Loading....</h1>
+}else{
 
   return (
     <div>
-      <PostPage posts={posts}/>
+      <PostPage posts={posts} />
     </div>
-  )
+  );
 }
 
-export default App
+}
+
+export default App;
